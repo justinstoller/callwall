11009a415263c1d4f580d9c462978d05774f9487
Justin Stoller <justin.stoller@gmail.com>
2011-07-22 22:21:14 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callwall::Application.config.secret_token = '95ecd6893b243380d6e0fb40a74be4f88477cdbaff1263426c9c31fa07c878d8500f410a11b3b79dfa852c2137f2ed4aec424ee8a22473256da5588fd2e8bcdc'
