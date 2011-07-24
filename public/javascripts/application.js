fe6f520e0aeb8a6a4042aea71631c2264884c3b7
Justin Stoller <justin.stoller@gmail.com>
2011-07-23 23:10:55 -0700

    javascript is ajaxy

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,39 +1,43 @@
-$(function() {
+(function($) {
+  $(document).ready(function() {
+    var server_domain = 'http://localhost:3000/';
+    var speek_domain = 'http://api.speek.com/calls/';
+    var organizer = '';
+    console.log("document ready");
 
-  var server_domain = 'http://localhost:3000/';
-  var speek_domain = 'http://api.speek.com/calls/';
-  var organizer = '';
-
-  $('#search').submit(function() {
-    organizer = $('#organizer');
-    $.get({
-      url: search_domain + 'search',
-      data: {
-        zip: $('#zip').value
-      },
-      success: function(data) {
-       // build something with the object passed back from the server
-      }
+    $('#searchform').submit(function() {
+      organizer = $('#organizer');
+      console.log(organizer.to_str);
+      console.log($('#search-zip').value.to_str);
+      $.get({
+        url: server_domain.to_str + 'search',
+        data: {
+          zip: $('#search-zip').value.to_str
+        },
+        success: function(data) {
+         // build something with the object passed back from the server
+        }
+      });
+      return false;
     });
-    return false;
-  });
 
-  $('.call').click(function() {
-    number = $(this).number;
-    $.post({
-      url: speek_domain + 'callNow',
-      data: {
-        api_key: api_key,
-        format: "json",
-        description: "A Call Wall demo",
-        music_on_hold: 0,
-        organizer: organizer,
-        numbers: number
-      },
-      success: function(data) {
-       // build something with the object passed back from the server
-      }
+    $('.call').click(function() {
+      number = $(this).number;
+      $.post({
+        url: speek_domain + 'callNow',
+        data: {
+          api_key: api_key,
+          format: "json",
+          description: "A Call Wall demo",
+          music_on_hold: 0,
+          organizer: organizer,
+          numbers: number
+        },
+        success: function(data) {
+         // build something with the object passed back from the server
+        }
+      });
+      return false;
     });
-    return false;
   });
-});
+})(jQuery);
